3125219e16acee40c9473a6093b54033d91f5d80
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-05-12 23:05:06 -0300

    feat: pagina de criar em progresso

--- src/pages/Create/index.tsx
+++ src/pages/Create/index.tsx
@@ -1,16 +1,44 @@
-import {Form} from 'react-bootstrap';
+import {Button, Col, Container, Form, Row} from 'react-bootstrap';
+import InputText from '../../components/InputText';
 
 export default function Create() {
+    const onSubmit = (event: unknown) => {
+        (event as SubmitEvent).preventDefault();
+        console.log('hello');
+    };
+
     return (
-        <>
-            <Form.Label htmlFor="name">Nome</Form.Label>
-            <Form.Control
-                id="name"
-                aria-describedby="name-help"
-            />
-            <Form.Text id="name-help" muted>
-            Nome do carro
-            </Form.Text>
-        </>
+        <Form onSubmit={onSubmit}>
+            <Container>
+              <Row>
+                <Col>
+                  <InputText placeholder='Fiesta' id='name' label='Nome' description='Nome do carro' />
+                </Col>
+                <Col>
+                  <InputText placeholder='2023' type='number' min={1} id='year' label='Lançamento' description='Data de lançman' />
+                </Col>
+              </Row>
+            </Container>
+            <Container>
+              <Row>
+                <Col>
+                  <InputText placeholder='Ford' id='brand' label='Marca' description='Marca do carro' />
+                </Col>
+                <Col>
+                  <InputText placeholder='R$ 20000,00' type='number' min={1} id='price' label='Preço' description='Preço' />
+                </Col>
+                <Col>
+                  <InputText placeholder='2 anos' id='warranty' label='Garantia' description='Garantia' />
+                </Col>
+              </Row>
+            </Container>
+            <Container>
+              <InputText placeholder='Novo' id='status' label='Estado' description='Estado do carro' />
+            </Container>
+            <Container>
+              <InputText id='description' label='Descrição' description='Descrição do carro' />
+            </Container>
+            <Button type='submit'>Criar</Button>
+        </Form>
     );
 }
