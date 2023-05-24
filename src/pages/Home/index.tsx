e89bb4b0ec2b75e76279b0414f0fae3521161b9f
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-05-23 21:44:55 -0300

    feat: texto nas divs de criar e listar carros

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,20 +4,21 @@ import Footer from '../../components/Footer';
 import concessionaria from '../../../public/concessionaria.jpg';
 import concessionaria_2 from '../../../public/concessionaria-2.jpg';
 
+import './styles.css';
+import {Link} from 'react-router-dom';
+
 export default function Home() {
 
   return (
     <>
       <Header />
       <div className='flex'>
-        <div className='right'>
-          <a href="/create">
-            <img className='w-100' src={concessionaria} alt="" />
-          </a>
+        <div className='right left-image-block'>
+          <Link className='left-image-anchor' to="/create">Inserir carros</Link>
         </div>
-        <div className='left'>
+        <div className='left right-image-block'>
           <a href="/list">
-            <img className='w-100' src={concessionaria_2} alt="" />
+            <Link className='left-image-anchor' to="/list">Listar carros</Link>
           </a>
         </div>
       </div>
